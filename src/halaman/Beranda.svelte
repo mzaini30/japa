f477fe75773e6ec1cfcac915d09ccddd66582618
mzaini30 <muhzaini30@gmail.com>
2020-10-09 22:04:00 +0800

    upload

---
+++ src/halaman/Beranda.svelte
@@ -0,0 +1,13 @@
+<div class="container">
+	<br>
+	<center>
+		<p><strong>PRIMA SOLUTIONS CONSULTANT</strong></p>
+		<p>Konsultan Psikologi Bidang Pendidikan</p>
+		<br>
+		<p>BUKU SOAL</p>
+		<p>JAPA INTEREST SCALE</p>
+		<p>G-IV</p>
+		<br>
+		<p><a href="#/instruksi" class="btn btn-success">Baca instruksi</a></p>
+	</center>
+</div>
