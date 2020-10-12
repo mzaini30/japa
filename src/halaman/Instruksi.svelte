eb0bd71659d1ecf921d80c43649d19c231e58843
mzaini30 <muhzaini30@gmail.com>
2020-10-12 17:28:04 +0800

    :construction: Belum selesai
    Masih di bagian biodata

--- src/halaman/Instruksi.svelte
+++ src/halaman/Instruksi.svelte
@@ -2,8 +2,7 @@
 	<br>
 	<p>Selamat pagi adik-adik.</p>
 	<p>Dalam buku ini terdapat sejumlah pernyataan untuk mengungkap minat pada bidang tertentu. Terdapat 99 pilihan. Tugas adik-adik adalah membaca semua pilihan tersebut. Kemudian, pilihlah 7 (tujuh) dari 99 yang menggambarkan diri adik. Tulislah nomor yang menggambarkan pilihan di kertas yang disediakan. Kemudian, rangkinglah dari 1 sampai dengan 7. Nomor 1 menunjukkan yang paling diminati, nomor 2 diminati, nomor 3 cukup diminati, dst.</p>
-	<p class="text-center"><a href="#/tes/1" class="btn btn-info">Saya paham</a></p>
-	<h5>Biodata</h5>
+	<hr>
 	<form>
      <div class="form-group">
       <label for="nomor-tes">No Tes</label>
@@ -15,9 +14,20 @@
      </div>
      <div class="form-group">
       <label for="kelas-sekolah">Kelas / Sekolah</label>
-      <input id="kelas-sekolah" class="form-control">
+      <input id="kelas-sekolah" class="form-control" required>
      </div>
      <div class="form-group">
+      <label for='jk'>JK</label>
+      <input type="text" class="form-control" id="jk" required name="">
+     </div>
+     <div class="form-group text-center">
+       <input type="submit" class="btn btn-info" value="Lanjut..." name="">
      </div>
 	</form>
 </div>
+<script type="text/javascript">
+  import {push} from 'svelte-spa-router'
+  const lanjut = () => {
+
+  }
+</script>
